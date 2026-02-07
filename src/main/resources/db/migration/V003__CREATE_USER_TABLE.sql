c6df3217040f3858e85c8fb578d91584c4426a5b
na0p0n <00naopon00@gmail.com>
2026-02-07 20:27:27 +0900

    コメントの対応 - 冗長なインデックスのCREATEの削除

--- src/main/resources/db/migration/V003__CREATE_USER_TABLE.sql
+++ src/main/resources/db/migration/V003__CREATE_USER_TABLE.sql
@@ -8,6 +8,3 @@ CREATE TABLE users(
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE INDEX idx_users_email ON users(email);
-CREATE INDEX idx_users_username ON users(username);
