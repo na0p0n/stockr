8ebd7c77bc824a200521f27e61da2ecc78493278
na0p0n <00naopon00@gmail.com>
2026-02-08 12:59:32 +0900

    LoggerDelegateの作成

---
+++ src/main/kotlin/net/naoponju/stockr/common/LoggerDelegate.kt
@@ -0,0 +1,12 @@
+package net.naoponju.stockr.common
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class LoggerDelegate: ReadOnlyProperty<Any, Logger> {
+    override fun getValue(thisRef: Any, property: KProperty<*>): Logger {
+        return LoggerFactory.getLogger(thisRef.javaClass)
+    }
+}
