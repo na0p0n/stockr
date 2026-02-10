ca88ffbb19687b41dc73f4889a39ef82299c11da
Naopon <21a3038@sit.shonan-it.ac.jp>
2026-02-10 18:01:47 +0900

    ktlint導入

--- src/main/kotlin/net/naoponju/stockr/common/LoggerDelegate.kt
+++ src/main/kotlin/net/naoponju/stockr/common/LoggerDelegate.kt
@@ -5,8 +5,11 @@ import org.slf4j.LoggerFactory
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-object LoggerDelegate: ReadOnlyProperty<Any, Logger> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): Logger {
+object LoggerDelegate : ReadOnlyProperty<Any, Logger> {
+    override fun getValue(
+        thisRef: Any,
+        property: KProperty<*>,
+    ): Logger {
         val klass = thisRef.javaClass
         val loggerClass = klass.enclosingClass?.takeIf { it.kotlin.isCompanion } ?: klass
         return LoggerFactory.getLogger(loggerClass)
