aebed9f1a8294bf0b9af41747dd0a0b34fbc672f
Naopon <21a3038@sit.shonan-it.ac.jp>
2026-02-10 19:37:44 +0900

    コメント対応

--- src/main/kotlin/net/naoponju/stockr/present/config/SecurityConfigProd.kt
+++ src/main/kotlin/net/naoponju/stockr/present/config/SecurityConfigProd.kt
@@ -3,7 +3,6 @@ package net.naoponju.stockr.present.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
@@ -24,7 +23,6 @@ class SecurityConfigProd {
             authorizeHttpRequests {
                 authorize("/login", permitAll)
                 authorize("/css/**", permitAll)
-                authorize(HttpMethod.POST, "/api/user", permitAll)
                 authorize(anyRequest, authenticated) // 認証済みユーザーのみ許可
             }
             formLogin {
